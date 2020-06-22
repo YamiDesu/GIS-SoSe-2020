e8f6d25bca910807aacaac17921be4b41666118d
Justin Dretvic <OtakuBen.Drowned@gmail.com>
2020-06-22 17:59:02 +0200

    Fix Aufgabe 08

--- Aufgaben/Aufgabe_08_2020-06-17/server.js
+++ Aufgaben/Aufgabe_08_2020-06-17/server.js
@@ -4,25 +4,26 @@ exports.A008Server = void 0;
 const Http = require("http");
 var A008Server;
 (function (A008Server) {
-    console.log("Starting server");
-    let port = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
-    let server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    console.log("Starting server"); // Ausgabe in der Server-Console
+    let port = Number(process.env.PORT); /* Deklaration der number "port" mit einem Port aus der Umgebungsvariable,
+                                                                                                        der als numerisches Objekt abgespeichert werden soll*/
+    if (!port) // "Wenn kein Port vergeben wurde...
+        port = 8100; // ...verwende den Port 8100"
+    let server = Http.createServer(); // Der Server wird aufgesetzt
+    server.addListener("request", handleRequest); // Listener für wenn Serveranfragen reinkommen
+    server.addListener("listening", handleListen); // Listener für wenn der Server startet
+    server.listen(port); // Achtet auf Anfragen, die durch den Port durchkommen
     function handleListen() {
-        console.log("Listening");
+        console.log("Listening"); // Ausgabe in der Server-Console
     }
     function handleRequest(_request, _response) {
-        console.log("https://theoneandgis.herokuapp.com" + `${_request.url}`);
+        console.log("https://theoneandgis.herokuapp.com" + `${_request.url}`); // Ausgabe in der Server-Console
         // console.log("-------------");
         // console.log("I hear voices!");
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
-        _response.write(_request.url);
-        _response.end();
+        _response.setHeader("content-type", "text/html; charset=utf-8"); // Der Datentyp der Antwort wird festgelegt
+        _response.setHeader("Access-Control-Allow-Origin", "*"); // erlaubt beliebigen Anfragen aus dem Netz, die Antwort zu lesen
+        _response.write(_request.url); // Die Antwort wird mit einem string (in diesem Fall die URL) beschrieben
+        _response.end(); // deaktiviert den Server bis zur nächsten Anfrage
     }
 })(A008Server = exports.A008Server || (exports.A008Server = {}));
 //# sourceMappingURL=server.js.map
--- Aufgaben/Aufgabe_08_2020-06-17/server.ts
+++ Aufgaben/Aufgabe_08_2020-06-17/server.ts
@@ -1,30 +1,31 @@
 import * as Http from "http";
 
 export namespace A008Server {
-    console.log("Starting server");
-    let port: number = Number(process.env.PORT);
-    if (!port)
-        port = 8100;
+    console.log("Starting server");                                                                     // Ausgabe in der Server-Console
+    let port: number = Number(process.env.PORT);                                                        /* Deklaration der number "port" mit einem Port aus der Umgebungsvariable,
+                                                                                                        der als numerisches Objekt abgespeichert werden soll*/
+    if (!port)                                                                                          // "Wenn kein Port vergeben wurde...
+        port = 8100;                                                                                    // ...verwende den Port 8100"
 
-    let server: Http.Server = Http.createServer();
-    server.addListener("request", handleRequest);
-    server.addListener("listening", handleListen);
-    server.listen(port);
+    let server: Http.Server = Http.createServer();                                                      // Der Server wird aufgesetzt
+    server.addListener("request", handleRequest);                                                       // Listener für wenn Serveranfragen reinkommen
+    server.addListener("listening", handleListen);                                                      // Listener für wenn der Server startet
+    server.listen(port);                                                                                // Achtet auf Anfragen, die durch den Port durchkommen
 
-    function handleListen(): void {
-        console.log("Listening");
+    function handleListen(): void {                                                                     // wird ausgeführt, wenn der Server gestartet wird und eine Abfrage erwartet
+        console.log("Listening");                                                                       // Ausgabe in der Server-Console
     }
 
-    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {
-        console.log("https://theoneandgis.herokuapp.com" + `${_request.url}`);
+    function handleRequest(_request: Http.IncomingMessage, _response: Http.ServerResponse): void {      // wird ausgeführt, wenn eine Serveranfrage eintrifft
+        console.log("https://theoneandgis.herokuapp.com" + `${_request.url}`);                          // Ausgabe in der Server-Console
         // console.log("-------------");
         // console.log("I hear voices!");
 
-        _response.setHeader("content-type", "text/html; charset=utf-8");
-        _response.setHeader("Access-Control-Allow-Origin", "*");
+        _response.setHeader("content-type", "text/html; charset=utf-8");                                // Der Datentyp der Antwort wird festgelegt
+        _response.setHeader("Access-Control-Allow-Origin", "*");                                        // erlaubt beliebigen Anfragen aus dem Netz, die Antwort zu lesen
 
-        _response.write(_request.url);
+        _response.write(_request.url);                                                                  // Die Antwort wird mit einem string (in diesem Fall die URL) beschrieben
 
-        _response.end();
+        _response.end();                                                                                // deaktiviert den Server bis zur nächsten Anfrage
     }
 }
